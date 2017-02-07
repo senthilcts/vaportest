6788515ab2619ba05a130bb2198f8a271c696d11
Senthil <thendral@histoadmins-iMac-3.local>
2017-02-07 18:47:15 +0530

    New get

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -8,6 +8,13 @@ drop.get { req in
     ])
 }
 
+drop.get("/name",":name") { request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
